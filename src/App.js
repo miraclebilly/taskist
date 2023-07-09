7c91bf044637c5d3ebd8854728ed4b304a40b0c4
Miracle Billy <uniquemiraculous@gmail.com>
2023-07-10 00:44:21 +0100

    local storage added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-
-import React from 'react'
+import React, { useEffect } from 'react';
 import './App.css';
-import Nav from './nav'
-import TaskList from './TaskList'
-import Add from './Add'
-
-
+import Nav from './nav';
+import TaskList from './TaskList';
+import Add from './Add';
 
 function App() {
-  const [tasks, setTasks] = React.useState([]);
- 
+  const [tasks, setTasks] = React.useState(null);
 
-    const handleAddTask = (task) => {
-        setTasks([...tasks, task]);
-    };
-
-    const handleEditTask= (index, editedTask) => {
-        const updatedTasks = [...tasks];
-        updatedTasks[index] = editedTask;
-        setTasks(updatedTasks);
+  useEffect(() => {
+    if (tasks === null) {
+      const storedTasks = localStorage.getItem('tasks');
+      if (storedTasks) {
+        setTasks(JSON.parse(storedTasks));
+      } else {
+        setTasks([]);
+      }
+    } else {
+      // Store the tasks in local storage whenever the tasks state changes
+      localStorage.setItem('tasks', JSON.stringify(tasks));
     }
+  }, [tasks]);
 
-    const deleteTask = (index) => {
-        const updatedTasks = [...tasks];
-        updatedTasks.splice(index, 1);
-        setTasks(updatedTasks);
-    };
+  const handleAddTask = (task) => {
+    setTasks([...tasks, task]);
+  };
 
-    
+  const handleEditTask = (index, editedTask) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = editedTask;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, taskIndex) => taskIndex !== index));
+  };
 
   return (
     <div>
       <Nav />
-            <Add onAdd={handleAddTask}/> 
-            <TaskList tasks={tasks} onEdit={handleEditTask} deleteTask={deleteTask}/> 
-      
-  </div>
+      <Add onAdd={handleAddTask} />
+      {tasks && <TaskList tasks={tasks} onEdit={handleEditTask} deleteTask={deleteTask} />}
+    </div>
   );
 }
 
